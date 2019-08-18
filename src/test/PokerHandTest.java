2177e509b12e99da7997dbc831884689f1791ce4
lmy <549408221@qq.com>
2019-08-18 14:57:38 +0800

    fourth test : multicard compare

--- src/test/PokerHandTest.java
+++ src/test/PokerHandTest.java
@@ -24,4 +24,9 @@ public class PokerHandTest {
         String actual = pokerHand.play("7C", "TD");
         assertEquals(pokerHand.PLAYER_2_WIN, actual);
     }
+    @Test
+    public void should_return_player_1_win_when_input_3S_9H_KC_5D_1S_and_5S_JH_9C_3H_1H() {
+        String actual = pokerHand.play("3S 9H KC 5D 1S", "5S JH 9C 3H 1H");
+        assertEquals(PokerHand.PLAYER_1_WIN, actual);
+    }
 }
