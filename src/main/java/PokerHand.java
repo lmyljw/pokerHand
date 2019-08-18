095d0934d16d32a357421ecc79b15bc7768c1bc7
lmy <549408221@qq.com>
2019-08-18 14:30:23 +0800

    second test impl

--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -11,8 +11,13 @@ public class PokerHand {
         int card1Number = Integer.parseInt(card1.substring(0, 1));
         int card2Number = Integer.parseInt(card2.substring(0, 1));
 
-        String result=card1Number > card2Number?PLAYER_1_WIN:PLAYER_2_WIN;
+        if (card1Number > card2Number) {
+            return PLAYER_1_WIN;
+        } else if (card1Number < card2Number) {
+            return PLAYER_2_WIN;
+        } else {
+            return PEACE;
+        }
 
-        return result;
     }
 }
