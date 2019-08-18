307c3cef5db28f55afbe57459e2c1d8897bc0d1f
lmy <549408221@qq.com>
2019-08-18 14:49:38 +0800

    rename exec to play

--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -6,7 +6,7 @@ public class PokerHand {
     public final static String PLAYER_2_WIN = "Player 2 Win";
     public final static String PEACE = "Peace";
 
-    public String exec(String card1, String card2) {
+    public String play(String card1, String card2) {
 
         Poker player1Poker = new Poker(card1.substring(0,1),card1.substring(1,2));
         Poker player2Poker = new Poker(card2.substring(0,1),card2.substring(1,2));
--- src/test/PokerHandTest.java
+++ src/test/PokerHandTest.java
@@ -11,17 +11,17 @@ public class PokerHandTest {
 
     @Test
     public void should_return_player_1_win_when_input_7C_and_5D() {
-        String actual = pokerHand.exec("7C", "5D");
+        String actual = pokerHand.play("7C", "5D");
         assertEquals(pokerHand.PLAYER_1_WIN, actual);
     }
     @Test
     public void should_return_peace_when_input_7C_and_7D() {
-        String actual = pokerHand.exec("7C", "7D");
+        String actual = pokerHand.play("7C", "7D");
         assertEquals(pokerHand.PEACE, actual);
     }
     @Test
     public void should_return_player_2_win_when_input_7C_and_TD() {
-        String actual = pokerHand.exec("7C", "TD");
+        String actual = pokerHand.play("7C", "TD");
         assertEquals(pokerHand.PLAYER_2_WIN, actual);
     }
 }
