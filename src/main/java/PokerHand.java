5f08733fbcfdcaba396184daa09c408edf78cd45
lmy <549408221@qq.com>
2019-08-18 14:20:14 +0800

    first test impl

--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -6,6 +6,12 @@ public class PokerHand {
     public final static String PLAYER_2_WIN = "Player 2 Win";
 
     public String exec(String card1, String card2) {
-        return "";
+
+        int card1Number = Integer.parseInt(card1.substring(0, 1));
+        int card2Number = Integer.parseInt(card2.substring(0, 1));
+
+        String result=card1Number > card2Number?PLAYER_1_WIN:PLAYER_2_WIN;
+
+        return result;
     }
 }
