cdee8874f6053863cae765485e2d3d6e909b3c6d
lmy <549408221@qq.com>
2019-08-18 14:32:39 +0800

    third test: T J Q K card

--- src/test/PokerHandTest.java
+++ src/test/PokerHandTest.java
@@ -10,13 +10,18 @@ public class PokerHandTest {
     private PokerHand pokerHand = new PokerHand();
 
     @Test
-    public void should_return_1th_player_when_input_7C_and_5D() {
+    public void should_return_player_1_win_when_input_7C_and_5D() {
         String actual = pokerHand.exec("7C", "5D");
         assertEquals(pokerHand.PLAYER_1_WIN, actual);
     }
     @Test
     public void should_return_peace_when_input_7C_and_7D() {
         String actual = pokerHand.exec("7C", "7D");
+        assertEquals(pokerHand.PEACE, actual);
+    }
+    @Test
+    public void should_return_player_2_win_when_input_7C_and_TD() {
+        String actual = pokerHand.exec("7C", "TD");
         assertEquals(pokerHand.PEACE, actual);
     }
 }
